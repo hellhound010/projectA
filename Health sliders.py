d22b231c2fbb95f597969b2e74f682c9f0e63497
hellhound010 <45426886+hellhound010@users.noreply.github.com>
2018-12-10 11:23:58 +0100

    Update Health sliders.py

--- Health sliders.py
+++ Health sliders.py
@@ -1,5 +1,11 @@
 import turtle as t
 
+t.hideturtle()
+t.bgcolor('black')
+t.color('white')
+t.Screen()
+
+
 players = {'player1_hp':100,
            'player1_sp':0,
            'player2_hp':100,
@@ -16,14 +22,26 @@ players = {'player1_hp':100,
 
 amount_players = t.numinput('Free For All!','How many players are there?', default=4, minval=2, maxval=6)
 
+t.penup()
+
+t.setpos(-453.55,290.55)
+
+
+
 def playerStats():
     global amount_players
     for i in range(int(amount_players)):
         x = i + 1
-        print('Player ' + str(x) + ' HP: ' + str(players['player' + str(x) + '_hp']) + '\nPlayer ' + str(x) +
-              ' SP: ' + str(players['player' + str(x) + '_sp']))
-        print('')
+        t.penup()
+        t.write('Player ' + str(x) + ' HP: ' + str(players['player' + str(x) + '_hp']) + '\nPlayer ' + str(x) +
+              ' SP: ' + str(players['player' + str(x) + '_sp']),font=("Arial", 14, "normal"))
+        t.seth(270)
+        t.fd(150)
+
+
+
 
 
 playerStats()
 
+t.mainloop()
