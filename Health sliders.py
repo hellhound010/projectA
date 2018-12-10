5d00516afe200a369bc3cf8bf45cad6dc1cae302
hellhound010 <45426886+hellhound010@users.noreply.github.com>
2018-12-10 11:19:34 +0100

    Health sliders

---
+++ Health sliders.py
@@ -0,0 +1,29 @@
+import turtle as t
+
+players = {'player1_hp':100,
+           'player1_sp':0,
+           'player2_hp':100,
+           'player2_sp':0,
+           'player3_hp':100,
+           'player3_sp':0,
+           'player4_hp':100,
+           'player4_sp':0,
+           'player5_hp':100,
+           'player5_sp':0,
+           'player6_hp':100,
+           'player6_sp':0
+           }
+
+amount_players = t.numinput('Free For All!','How many players are there?', default=4, minval=2, maxval=6)
+
+def playerStats():
+    global amount_players
+    for i in range(int(amount_players)):
+        x = i + 1
+        print('Player ' + str(x) + ' HP: ' + str(players['player' + str(x) + '_hp']) + '\nPlayer ' + str(x) +
+              ' SP: ' + str(players['player' + str(x) + '_sp']))
+        print('')
+
+
+playerStats()
+
