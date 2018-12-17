6f78ec4ca3aa952dddff881b43316b83cc0753ba
Speedie778 <43468859+Speedie778@users.noreply.github.com>
2018-12-17 16:08:00 +0100

    Health Sliders Basic
    This version of the Health Sliders works entirely. Once implemented in the Master Doc, it has to be optimized.

--- Health sliders.py
+++ Health sliders.py
@@ -1,9 +1,12 @@
 import turtle as t
 
-t.hideturtle()
-t.bgcolor('black')
-t.color('white')
-t.Screen()
+t.Screen() #Preparations for this module. Remove or adjust in main code.
+t.seth(270)
+t.penup()
+t.tracer(0,0) #Screen Stabilizer
+stats_Start_Coords = -330,265
+t.update()
+t.goto(stats_Start_Coords)
 
 
 players = {'player1_hp':100,
@@ -21,27 +24,60 @@ players = {'player1_hp':100,
            }
 
 amount_players = t.numinput('Free For All!','How many players are there?', default=4, minval=2, maxval=6)
-
-t.penup()
-
-t.setpos(-453.55,290.55)
-
-
+while amount_players == None:
+  amount_players = t.numinput('Free For All!','How many players are there?', default=4, minval=2, maxval=6)  
 
 def playerStats():
     global amount_players
     for i in range(int(amount_players)):
         x = i + 1
-        t.penup()
-        t.write('Player ' + str(x) + ' HP: ' + str(players['player' + str(x) + '_hp']) + '\nPlayer ' + str(x) +
-              ' SP: ' + str(players['player' + str(x) + '_sp']),font=("Arial", 14, "normal"))
-        t.seth(270)
-        t.fd(150)
+        t.write('Player ' + str(x) + ' HP: ' + str(players['player' + str(x) + '_hp']), font=18)
+        t.fd(20)
+        t.write('Player ' + str(x) +' SP: ' + str(players['player' + str(x) + '_sp']), font=18)
+        t.fd(40)
+        print('')
 
+def damageTake():
+  damaged_Player = t.numinput('Free for All!','Which player took damage?', default = 3, minval = 1, maxval = amount_players)
+  if damaged_Player == None:
+    t.onkeypress(damageTake,'w')
+    t.listen()
+    return 'Empty'
+  dmg = t.numinput('Free for All!','How much damage did they take?', default = 0, minval = 0, maxval = 100)
+  if dmg == None:
+    t.onkeypress(damageTake,'w')
+    t.listen()
+    return 'Empty'
+  players['player' + str(int(damaged_Player)) + '_hp'] = players['player' + str(int(damaged_Player)) + '_hp'] - int(dmg)
+  if players['player' + str(int(damaged_Player)) + '_hp'] < 0:
+    players['player' + str(int(damaged_Player)) + '_hp'] = 0
+  t.penup()
+  t.goto(stats_Start_Coords)
+  t.clear()
+  playerStats() 
+  t.onkeypress(damageTake,'w')
+  t.listen()
 
-
-
+def shieldGive():
+  shield = 25
+  shielded_Player = t.numinput('Free for All!','Which player got shield', default = 3, minval = 1, maxval = amount_players)
+  if shielded_Player == None:
+    t.onkeypress(ShieldGive,'q')
+    t.listen()
+    return 'No Shield Given'
+  players['player' + str(int(shielded_Player)) + '_sp'] = players['player' + str(int(shielded_Player)) + '_sp'] + shield
+  if players['player' + str(int(shielded_Player)) + '_sp'] >= 100:
+    players['player' + str(int(shielded_Player)) + '_sp'] = 100
+  t.penup()
+  t.goto(stats_Start_Coords)
+  t.clear()
+  playerStats() 
+  t.onkeypress(shieldGive,'q')
+  t.listen()
 
 playerStats()
+t.onkeypress(damageTake,'w')
+t.onkeypress(shieldGive,'q')
+t.listen()
 
-t.mainloop()
+t.mainloop() #Keeping Test screen. Remove in main code.
